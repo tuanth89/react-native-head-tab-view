44571aa9b004dde622bd8e8d141ff3d744abb4bf
trantuan <trantuan101989@gmail.com>
2021-01-25 08:44:12 +0700

    Re-commit Fixed: - keyboardpersit

--- Example/react-native-head-tab-view/index.js
+++
@@ -1,6 +0,0 @@
-
-export {default as TabView} from './TabView'
-export {default as Tabbar} from './Tabbar'
-export {default as HPageViewHoc} from './HPageViewHoc'
-export {default as RefreshControlAnimated} from './RefreshControlAnimated'
-
---
+++ index.js
@@ -0,0 +1,6 @@
+
+export {default as TabView} from './TabView'
+export {default as Tabbar} from './Tabbar'
+export {default as HPageViewHoc} from './HPageViewHoc'
+export {default as RefreshControlAnimated} from './RefreshControlAnimated'
+
